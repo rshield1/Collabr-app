2a334120773bd53ef3a6612037620b0120fc2dfb
Robert Shields <you@example.com>
2021-11-14 00:16:07 -0800

    customizing chatarea

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
 
 import './App.css';
+import ChatFeed from './components/ChatFeed';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="75289545-5f69-43a2-a84c-91a212a6877a"
+            userName="Admin"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+        />
+    )
+}
+
+export default App
